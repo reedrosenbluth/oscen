705b7f04f19c4e7e020a951f65c4d01233ee3ed4
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2020-04-01 13:16:50 -0600

    refactor complete

--- src/main.rs
+++ src/main.rs
@@ -13,94 +13,162 @@ fn main() {
 }
 
 trait Wave {
-    fn sample(&self, t: f64) -> f32;
+    fn sample(&self) -> f32;
+    fn update_phase(&mut self, sample_rate: f64);
+    fn hz(&self) -> f64;
+    fn set_hz(&mut self, hz: f64);
 }
+
 
 #[derive(Constructor)]
 struct WaveParams {
     hz: f64,
     volume: f32,
+    phase: f64,
 }
 
 struct SineWave(WaveParams);
 
 impl SineWave {
-    fn new(hz: f64, volume: f32) -> Self {
-        SineWave(WaveParams::new(hz, volume))
+    fn new(hz: f64, volume: f32, phase: f64) -> Self {
+        SineWave(WaveParams::new(hz, volume, phase))
     }
 }
 
 impl Wave for SineWave {
-    fn sample(&self, t: f64) -> f32 {
-        let t = t * self.0.hz;
-        self.0.volume * (2. * PI * t).sin() as f32
+    fn sample(&self) -> f32 {
+        self.0.volume * (2. * PI * self.0.phase).sin() as f32
+    }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.0.phase += self.0.hz / sample_rate;
+        self.0.phase %= sample_rate;
+    }
+
+    fn hz(&self) -> f64 {
+        self.0.hz
+    }
+
+    fn set_hz(&mut self, hz: f64) {
+        self.0.hz = hz;
     }
 }
 
 struct SquareWave(WaveParams);
 
 impl SquareWave {
-    fn new(hz: f64, volume: f32) -> Self {
-        SquareWave(WaveParams::new(hz, volume))
+    fn new(hz: f64, volume: f32, phase: f64) -> Self {
+        SquareWave(WaveParams::new(hz, volume, phase))
     }
 }
 
 impl Wave for SquareWave {
-    fn sample(&self, t: f64) -> f32 {
-        let sine_wave = SineWave(WaveParams::new(self.0.hz, self.0.volume));
-        let sine_amp = sine_wave.sample(t);
+    fn sample(&self) -> f32 {
+        let sine_wave = SineWave(WaveParams::new(self.0.hz, self.0.volume, self.0.phase));
+        let sine_amp = sine_wave.sample();
         if sine_amp > 0. {
             self.0.volume
         } else {
             -self.0.volume
         }
     }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.0.phase += self.0.hz / sample_rate;
+        self.0.phase %= sample_rate;
+    }
+
+    fn hz(&self) -> f64 {
+        self.0.hz
+    }
+    
+    fn set_hz(&mut self, hz: f64) {
+        self.0.hz = hz;
+    }
 }
 
 struct RampWave(WaveParams);
 
 impl RampWave {
-    fn new(hz: f64, volume: f32) -> Self {
-        RampWave(WaveParams::new(hz, volume))
+    fn new(hz: f64, volume: f32, phase: f64) -> Self {
+        RampWave(WaveParams::new(hz, volume, phase))
     }
 }
 
 impl Wave for RampWave {
-    fn sample(&self, t: f64) -> f32 {
-        let t = t * self.0.hz;
-        self.0.volume * (2. * (t - floor(0.5 + t, 0))) as f32
+    fn sample(&self) -> f32 {
+        self.0.volume * (2. * (self.0.phase - floor(0.5 + self.0.phase, 0))) as f32
+    }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.0.phase += self.0.hz / sample_rate;
+        self.0.phase %= sample_rate;
+    }
+
+    fn hz(&self) -> f64 {
+        self.0.hz
+    }
+
+    fn set_hz(&mut self, hz: f64) {
+        self.0.hz = hz;
     }
 }
 
 struct SawWave(WaveParams);
 
 impl SawWave {
-    fn new(hz: f64, volume: f32) -> Self {
-        SawWave(WaveParams::new(hz, volume))
+    fn new(hz: f64, volume: f32, phase: f64) -> Self {
+        SawWave(WaveParams::new(hz, volume, phase))
     }
 }
 
 impl Wave for SawWave {
-    fn sample(&self, t: f64) -> f32 {
-        let ramp_wave = RampWave(WaveParams::new(self.0.hz, self.0.volume));
-        let ramp_amp = ramp_wave.sample(t);
-        self.0.volume - ramp_amp
+    fn sample(&self) -> f32 {
+        let t = self.0.phase - 0.5;
+        self.0.volume * (2. * (-t - floor(0.5 - t, 0))) as f32
+    }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.0.phase += self.0.hz / sample_rate;
+        self.0.phase %= sample_rate;
+    }
+
+    fn hz(&self) -> f64 {
+        self.0.hz
+    }
+
+    fn set_hz(&mut self, hz: f64) {
+        self.0.hz = hz;
     }
 }
 
 struct TriangleWave(WaveParams);
 
 impl TriangleWave {
-    fn new(hz: f64, volume: f32) -> Self {
-        TriangleWave(WaveParams::new(hz, volume))
+    fn new(hz: f64, volume: f32, phase: f64) -> Self {
+        TriangleWave(WaveParams::new(hz, volume, phase))
     }
 }
 
 impl Wave for TriangleWave {
-    fn sample(&self, t: f64) -> f32 {
-        let saw_wave = SawWave(WaveParams::new(self.0.hz, self.0.volume));
-        let saw_amp = saw_wave.sample(t);
+    fn sample(&self) -> f32 {
+        let saw_wave = SawWave(WaveParams::new(self.0.hz, self.0.volume, self.0.phase));
+        let t = self.0.phase - 0.75;
+        let saw_amp = (2. * (-t - floor(0.5 - t, 0))) as f32;
         2. * saw_amp.abs() - self.0.volume
+    }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.0.phase += self.0.hz / sample_rate;
+        self.0.phase %= sample_rate;
+    }
+
+    fn hz(&self) -> f64 {
+        self.0.hz
+    }
+
+    fn set_hz(&mut self, hz: f64) {
+        self.0.hz = hz;
     }
 }
 
@@ -112,10 +180,28 @@ struct LerpWave {
 }
 
 impl Wave for LerpWave {
-    fn sample(&self, t: f64) -> f32 {
-        (1. - self.alpha) * self.wave1.sample(t) + self.alpha * self.wave2.sample(t)
+    fn sample(&self) -> f32 {
+        (1. - self.alpha) * self.wave1.sample() + self.alpha * self.wave2.sample()
     }
+
+    fn update_phase(&mut self, sample_rate: f64) {
+        self.wave1.update_phase(sample_rate);
+        self.wave2.update_phase(sample_rate);
+    }
+
+    fn hz(&self) -> f64 {
+        if self.wave1.hz() != self.wave2.hz() {
+            panic!("Can't interpolate between waves of different frequencies.")
+        }
+        self.wave1.hz()
+    }
+
+    fn set_hz(&mut self, hz: f64) {
+        self.wave1.set_hz(hz);
+        self.wave2.set_hz(hz);
+    } 
 }
+
 
 #[derive(Constructor)]
 struct Model {
@@ -129,7 +215,6 @@ struct Model {
 struct Synth {
     voice: Box<dyn Wave + Send>,
     sender: Sender<f32>,
-    clock: f64,
 }
 
 fn model(app: &App) -> Model {
@@ -147,8 +232,8 @@ fn model(app: &App) -> Model {
     // Initialise the audio API so we can spawn an audio stream.
     let audio_host = audio::Host::new();
     // Initialise the state that we want to live on the audio thread.
-    let wave1 = Box::new(SineWave::new(130.81, 0.5));
-    let wave2 = Box::new(SquareWave::new(130.81, 0.5));
+    let wave1 = Box::new(SineWave::new(130.81, 0.5, 0.0));
+    let wave2 = Box::new(SquareWave::new(130.81, 0.5, 0.0));
     let voice = LerpWave {
         wave1,
         wave2,
@@ -157,7 +242,6 @@ fn model(app: &App) -> Model {
     let model = Synth {
         voice: Box::new(voice),
         sender,
-        clock: 0.,
     };
     let stream = audio_host
         .new_output_stream(model)
@@ -178,9 +262,8 @@ fn audio(synth: &mut Synth, buffer: &mut Buffer) {
     let sample_rate = buffer.sample_rate() as f64;
     for frame in buffer.frames_mut() {
         let mut amp = 0.;
-        amp += synth.voice.sample(synth.clock);
-        synth.clock += 1. / sample_rate;
-        synth.clock %= sample_rate;
+        amp += synth.voice.sample();
+        synth.voice.update_phase(sample_rate);
         for channel in frame {
             *channel = amp;
         }
@@ -198,6 +281,29 @@ fn key_pressed(_app: &App, model: &mut Model, key: Key) {
             } else {
                 model.stream.play().unwrap();
             }
+        }
+        // Raise the frequency when the up key is pressed.
+        Key::Up => {
+            model
+                .stream
+                .send(|synth| {
+                        let start_freq = synth.voice.hz();
+                        let new_freq = start_freq * (2.0.powf(1. / 12.));
+                        synth.voice.set_hz(new_freq);
+                    }
+                )
+                .unwrap();
+        }
+        Key::Down => {
+            model
+                .stream
+                .send(|synth| {
+                        let start_freq = synth.voice.hz();
+                        let new_freq = start_freq * (2.0.powf(-1. / 12.));
+                        synth.voice.set_hz(new_freq);
+                    }
+                )
+                .unwrap();
         }
         _ => {}
     }
