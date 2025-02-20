8e2ed7e8b6d9c7878c4f238a95957a505a43b359
Reed Rosenbluth <331327+reedrosenbluth@users.noreply.github.com>
2025-02-20 10:24:44 -0500

    egui

--- cpal-apps/src/bin/synth2.rs
+++ cpal-apps/src/bin/synth2.rs
@@ -1,8 +1,5 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use iced::{
-    widget::{column, slider, text, Container},
-    Alignment, Application, Command, Element, Settings, Theme,
-};
+use eframe::egui;
 use oscen2::{EndpointType, Graph, Oscillator, OutputEndpoint, TPT_Filter, ValueKey};
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
@@ -19,7 +16,7 @@ impl Default for SynthParams {
     fn default() -> Self {
         Self {
             carrier_frequency: 440.0,
-            modulator_frequency: 0.5,
+            modulator_frequency: 100.0,
             cutoff_frequency: 3000.0,
             q_factor: 0.707,
         }
@@ -37,18 +34,22 @@ fn audio_callback(
     rx: &std::sync::mpsc::Receiver<SynthParams>,
     channels: usize,
 ) {
-    if let Ok(params) = rx.try_recv() {
+    let mut latest_params = None;
+    while let Ok(params) = rx.try_recv() {
+        latest_params = Some(params);
+    }
+
+    if let Some(params) = latest_params {
         graph.set_value(*carrier_freq_input, params.carrier_frequency, 1000);
         graph.set_value(*modulator_freq_input, params.modulator_frequency, 1000);
-        graph.set_value(*cutoff_freq_input, params.cutoff_frequency, 100);
-        graph.set_value(*q_input, params.q_factor, 100);
+        graph.set_value(*cutoff_freq_input, params.cutoff_frequency, 1000);
+        graph.set_value(*q_input, params.q_factor, 1000);
     }
 
     for frame in data.chunks_mut(channels) {
         graph.process();
 
         if let Some(value) = graph.get_value(output) {
-            // println!("Output value: {}", value);
             for sample in frame.iter_mut() {
                 *sample = value;
             }
@@ -56,7 +57,110 @@ fn audio_callback(
     }
 }
 
-fn main() -> iced::Result {
+struct ESynthApp {
+    params: SynthParams,
+    tx: Sender<SynthParams>,
+}
+
+impl ESynthApp {
+    fn new(tx: Sender<SynthParams>) -> Self {
+        Self {
+            params: SynthParams::default(),
+            tx,
+        }
+    }
+}
+
+impl eframe::App for ESynthApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.group(|ui| {
+                    // ui.set_min_width(400.0);
+
+                    ui.vertical(|ui| {
+                        ui.heading("Oscillator");
+                        ui.add_space(20.0);
+
+                        // Carrier Frequency
+                        ui.label("Carrier Frequency");
+                        if ui
+                            .add(
+                                egui::Slider::new(
+                                    &mut self.params.carrier_frequency,
+                                    20.0..=2000.0,
+                                )
+                                .step_by(1.0),
+                            )
+                            .changed()
+                        {
+                            let _ = self.tx.send(self.params);
+                        }
+
+                        ui.add_space(10.0);
+
+                        // Modulator Frequency
+                        ui.label("Modulator Frequency");
+                        if ui
+                            .add(
+                                egui::Slider::new(
+                                    &mut self.params.modulator_frequency,
+                                    20.0..=2000.0,
+                                )
+                                .step_by(0.1),
+                            )
+                            .changed()
+                        {
+                            let _ = self.tx.send(self.params);
+                        }
+                    });
+                });
+
+                ui.add_space(20.0);
+
+                ui.group(|ui| {
+                    // ui.set_min_width(400.0);
+                    ui.vertical(|ui| {
+                        ui.heading("Filter");
+                        ui.add_space(20.0);
+
+                        // Filter Cutoff
+                        ui.label("Filter Cutoff");
+                        if ui
+                            .add(
+                                egui::Slider::new(
+                                    &mut self.params.cutoff_frequency,
+                                    20.0..=20000.0,
+                                )
+                                .logarithmic(true)
+                                .step_by(0.1),
+                            )
+                            .changed()
+                        {
+                            let _ = self.tx.send(self.params);
+                        }
+
+                        ui.add_space(10.0);
+
+                        // Filter Q
+                        ui.label("Filter Q");
+                        if ui
+                            .add(
+                                egui::Slider::new(&mut self.params.q_factor, 0.1..=10.0)
+                                    .step_by(0.1),
+                            )
+                            .changed()
+                        {
+                            let _ = self.tx.send(self.params);
+                        }
+                    });
+                });
+            });
+        });
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
     let (tx, rx) = channel();
 
     thread::spawn(move || {
@@ -73,15 +177,15 @@ fn main() -> iced::Result {
 
         let mut graph = Graph::new(sample_rate);
 
-        let modulator = graph.add_node(Oscillator::sine(0.5, 0.5));
-        let carrier = graph.add_node(Oscillator::saw(440.0, 1.0));
+        let modulator = graph.add_node(Oscillator::sine(100.0, 0.5));
+        let carrier = graph.add_node(Oscillator::sine(440.0, 1.0));
         let filter = graph.add_node(TPT_Filter::new(3000.0, 0.707));
-        graph.connect(modulator.output(), carrier.frequency_mod());
+
+        graph.connect(modulator.output(), carrier.phase());
         graph.connect(carrier.output(), filter.input());
 
         let output = graph.transform(filter.output(), |x| x.tanh() * 0.5);
 
-        // TODO: don't love the input names as strings
         let carrier_freq_input = graph
             .get_input_by_name(carrier.node_key(), "frequency")
             .expect("Oscillator should have frequency input");
@@ -95,13 +199,12 @@ fn main() -> iced::Result {
             .get_input_by_name(filter.node_key(), "q")
             .expect("LPF should have Q input");
 
-        // Set up the frequency inputs as a Value endpoints
         graph
             .endpoint_types
             .insert(carrier_freq_input, EndpointType::value(440.0));
         graph
             .endpoint_types
-            .insert(modulator_freq_input, EndpointType::value(0.5));
+            .insert(modulator_freq_input, EndpointType::value(100.0));
         graph
             .endpoint_types
             .insert(cutoff_freq_input, EndpointType::value(3000.0));
@@ -138,116 +241,14 @@ fn main() -> iced::Result {
         }
     });
 
-    let mut settings = Settings::with_flags(tx);
-    settings.window.size = (300, 400);
-    Model::run(settings)
-}
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([370.0, 160.0]),
+        ..Default::default()
+    };
 
-struct Model {
-    params: SynthParams,
-    tx: Sender<SynthParams>,
-}
-
-#[derive(Debug, Clone)]
-enum Message {
-    SetCarrierFrequency(f32),
-    SetModulatorFrequency(f32),
-    SetCutoffFrequency(f32),
-    SetQFactor(f32),
-}
-
-impl Application for Model {
-    type Message = Message;
-    type Theme = Theme;
-    type Executor = iced::executor::Default;
-    type Flags = Sender<SynthParams>;
-
-    fn new(flags: Sender<SynthParams>) -> (Model, Command<Message>) {
-        (
-            Model {
-                params: SynthParams::default(),
-                tx: flags,
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("Oscillator Control")
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::SetCarrierFrequency(new_freq) => {
-                self.params.carrier_frequency = new_freq;
-            }
-            Message::SetModulatorFrequency(new_freq) => {
-                self.params.modulator_frequency = new_freq;
-            }
-            Message::SetCutoffFrequency(new_freq) => {
-                self.params.cutoff_frequency = new_freq;
-            }
-            Message::SetQFactor(new_q) => {
-                self.params.q_factor = new_q;
-            }
-        }
-        let _ = self.tx.send(self.params);
-        Command::none()
-    }
-
-    fn view(&self) -> Element<Message> {
-        Container::new(
-            column![
-                text(format!(
-                    "Carrier Frequency: {:.1} Hz",
-                    self.params.carrier_frequency
-                ))
-                .size(20),
-                slider(
-                    20.0..=2000.0,
-                    self.params.carrier_frequency,
-                    Message::SetCarrierFrequency
-                )
-                .step(1.0),
-                text(format!(
-                    "Modulator Frequency: {:.1} Hz",
-                    self.params.modulator_frequency
-                ))
-                .size(20),
-                slider(
-                    0.1..=40.0,
-                    self.params.modulator_frequency,
-                    Message::SetModulatorFrequency
-                )
-                .step(0.1),
-                text(format!(
-                    "Filter Cutoff: {:.1} Hz",
-                    self.params.cutoff_frequency
-                ))
-                .size(20),
-                slider(
-                    0.0..=1.0,
-                    linear_to_log(self.params.cutoff_frequency, 20.0, 20000.0),
-                    |norm| Message::SetCutoffFrequency(log_to_linear(norm, 20.0, 20000.0))
-                )
-                .step(0.01),
-                text(format!("Filter Q: {:.1} Hz", self.params.q_factor)).size(20),
-                slider(0.1..=10.0, self.params.q_factor, Message::SetQFactor).step(0.1),
-            ]
-            .spacing(20)
-            .align_items(Alignment::Center),
-        )
-        .padding(20)
-        .center_x()
-        .center_y()
-        .into()
-    }
-}
-
-fn linear_to_log(value: f32, min: f32, max: f32) -> f32 {
-    (value.ln() - min.ln()) / (max.ln() - min.ln())
-}
-
-fn log_to_linear(value: f32, min: f32, max: f32) -> f32 {
-    (value * (max.ln() - min.ln()) + min.ln()).exp()
+    eframe::run_native(
+        "Oscen v2",
+        options,
+        Box::new(|_cc| Ok(Box::new(ESynthApp::new(tx)))),
+    )
 }
